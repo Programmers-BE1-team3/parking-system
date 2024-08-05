7315ef8bd6fef9cab7cc4572ed0d6f8eeae64c6c
jbw9964 <jbw9964@gmail.com>
2024-08-05 22:05:23 +0900

    ParkRepository 추상 메서드 수정
    필요에 의해 아래 메서드 추가
    - `int updateEntrance(Park park)`
    - `int updateExit(Park park)`
    - `List<Park> selectAll()`
    
    메서드 삭제
    - `int updateIsParked`

--- src/repositories/ParkRepository.java
+++ src/repositories/ParkRepository.java
@@ -1,11 +1,14 @@
 package repositories;
 
 import domain.Car;
+import domain.Park;
+
+import java.util.List;
 
 public interface ParkRepository {
-
-    int insert(Car car);
-    List<AllParkInfo> selectAll();
-    int updateIsParked();
-
+    int update(Car car, int parking_slot_id);
+    int updateEntrance(Park park);
+    int updateExit(Park park);
+    Park selectBySlotId(int id);
+    List<Park> selectAll();
 }
