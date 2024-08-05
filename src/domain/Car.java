e260e15471f6ae4569db8ed4f23f7a73045a34c8
JeongIna <jeongina89@gmail.com>
2024-08-05 16:38:56 +0900

    Car,CarService 클래스 추가

--- src/domain/Car.java
+++ src/domain/Car.java
@@ -1,12 +1,12 @@
 package domain;
 
 public class Car {
-    private int car_id;
-    private String car_num;
-    private String car_type;
-    public Car(int car_id, String car_num, String car_type) {
-        this.car_id = car_id;
-        this.car_num = car_num;
-        this.car_type = car_type;
+    private int carId;
+    private String carNum;
+    private String carType;
+    public Car(int carId, String carNum, String carType) {
+        this.carId = carId;
+        this.carNum = carNum;
+        this.carType = carType;
     }
 }
--- src/repositories/CarRepository.java
+++ src/repositories/CarRepository.java
@@ -8,6 +8,6 @@ public interface CarRepository {
     int insert();
     int update(int id);
     Car select();
-    List<Car> selectAll();
+    List<Car> selectall();
 
 }
