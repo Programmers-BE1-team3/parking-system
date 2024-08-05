6b832a97c9519f851aa196975796af0b93c08e2d
JeongIna <jeongina89@gmail.com>
2024-08-05 16:29:44 +0900

    Car,CarService 클래스 추가

--- src/domain/Car.java
+++ src/domain/Car.java
@@ -1,5 +1,12 @@
 package domain;
 
 public class Car {
-
+    private int car_id;
+    private String car_num;
+    private String car_type;
+    public Car(int car_id, String car_num, String car_type) {
+        this.car_id = car_id;
+        this.car_num = car_num;
+        this.car_type = car_type;
+    }
 }
